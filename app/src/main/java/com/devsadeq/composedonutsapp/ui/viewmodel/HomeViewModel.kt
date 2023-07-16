e839761bf2244d3cb23850c18f5eb6d26e093be8
Sadeq Al-Mhana <64174395+devsadeq@users.noreply.github.com>
2023-07-16 23:04:30 +0300

    prepare fake data

---
+++ app/src/main/java/com/devsadeq/composedonutsapp/ui/viewmodel/HomeViewModel.kt
@@ -0,0 +1,34 @@
+package com.devsadeq.composedonutsapp.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.devsadeq.composedonutsapp.ui.DataSource
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import javax.inject.Inject
+
+@HiltViewModel
+class HomeViewModel @Inject constructor() : ViewModel() {
+    private val _state = MutableStateFlow(HomeUiState())
+    val state = _state.asStateFlow()
+
+    init {
+        getTodayOffers()
+        getDonuts()
+    }
+
+    private fun getTodayOffers() {
+        _state.update {
+            it.copy(
+                todayOffers = DataSource.getDonuts().filter { donut -> donut.isOffer }
+            )
+        }
+    }
+
+    private fun getDonuts() {
+        _state.update {
+            it.copy(donuts = DataSource.getDonuts())
+        }
+    }
+}
