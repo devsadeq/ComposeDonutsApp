e44c2edc64cf2b0af60d36b3cf1228026898e74f
Sadeq Al-Mhana <64174395+devsadeq@users.noreply.github.com>
2023-07-20 13:10:50 +0300

    fix bottom navigation

--- app/src/main/java/com/devsadeq/composedonutsapp/ui/app/DonutsApp.kt
+++ app/src/main/java/com/devsadeq/composedonutsapp/ui/app/DonutsApp.kt
@@ -1,6 +1,5 @@
 package com.devsadeq.composedonutsapp.ui.app
 
-import android.util.Log
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.height
@@ -11,11 +10,13 @@ import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.devsadeq.composedonutsapp.ui.navigation.DonutsNavGraph
 import com.devsadeq.composedonutsapp.ui.navigation.Screen
@@ -26,7 +27,7 @@ import com.devsadeq.composedonutsapp.ui.screen.onboarding.ON_BOARDING_ROUTE
 fun DonutsApp() {
     val navController = rememberNavController()
     Scaffold(
-        bottomBar = { DonutsBottomBar(navController = navController) }
+        bottomBar = { DonutsBottomBar(navController) }
     ) { innerPadding ->
         Box(modifier = Modifier.padding(innerPadding)) {
             DonutsNavGraph(navHostController = navController)
@@ -36,8 +37,7 @@ fun DonutsApp() {
 
 @Composable
 fun DonutsBottomBar(navController: NavHostController) {
-    Log.d("SADEQMHANA", "currentDestination: ${navController.currentDestination?.route}")
-    val currentDestination = navController.currentDestination?.route
+    val currentRoute by navController.currentBackStackEntryAsState()
     val items = listOf(
         Screen.Home,
         Screen.Favorites,
@@ -46,33 +46,26 @@ fun DonutsBottomBar(navController: NavHostController) {
         Screen.Profile,
     )
 
-    if (currentDestination != DETAILS_ROUTE && currentDestination != ON_BOARDING_ROUTE) BottomNavigation(
-        modifier = Modifier.height(80.dp),
-        backgroundColor = Color.White,
-        elevation = 0.dp,
-    ) {
-        items.forEach { screen ->
-            AddItem(
-                screen = screen,
-                currentDestination = currentDestination ?: "",
-            )
+    if (currentRoute?.destination?.route != "details/{id}" && currentRoute?.destination?.route != ON_BOARDING_ROUTE)
+        BottomNavigation(
+            modifier = Modifier.height(80.dp),
+            backgroundColor = Color.White,
+            elevation = 0.dp,
+        ) {
+            items.forEach { screen ->
+                AddItem(screen = screen)
+            }
         }
-    }
 }
 
 @Composable
-private fun RowScope.AddItem(
-    screen: Screen,
-    currentDestination: String,
-) {
-    val selected = screen.route == currentDestination
-    val icon = if (selected) screen.selectedIcon else screen.unselectedIcon
+private fun RowScope.AddItem(screen: Screen) {
     BottomNavigationItem(
-        selected = selected,
+        selected = true,
         onClick = {},
         icon = {
             Icon(
-                painter = painterResource(icon),
+                painter = painterResource(screen.selectedIcon),
                 contentDescription = null,
                 modifier = Modifier.size(36.dp),
                 tint = Color(0xFFFF7074)
